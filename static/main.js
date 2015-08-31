e70f4db209c0cd5e92e8d3787ed1c665973f7963
David Larrimore <davidlarrimore@gmail.com>
2015-08-30 21:51:34 -0400

    changed son_list to results

--- static/main.js
+++ static/main.js
@@ -5,7 +5,7 @@
     app.controller("UsersCtrl", function($scope, $http) {
       $http.get('/api/users').
         success(function(data, status, headers, config) {
-          $scope.users = data.json_list;
+          $scope.users = data.results;
         }).
         error(function(data, status, headers, config) {
           // log error
