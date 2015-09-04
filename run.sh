beb51ea290f5b369c9da759d4b762ebd9b6ac8ee
David Larrimore <davidlarrimore@gmail.com>
2015-09-04 10:08:02 -0400

    Updated Newrelic

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export APP_SETTINGS="config.DevelopmentConfig"
+export APP_SETTINGS='config.DevelopmentConfig'
 export DATABASE_URL='mysql://twoweeks:twoweeks@mixfindb.c6uo5ewdeq5k.us-east-1.rds.amazonaws.com:3306/twoweeks'
-export NEW_RELIC_CONFIG_FILE=newrelic.ini
+export NEW_RELIC_CONFIG_FILE='newrelic.ini'
 
-python twoweeks.py
+newrelic-admin run-program python twoweeks.py
