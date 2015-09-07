37a482b91b5f02edf65474561957bc31b8f613c8
David Larrimore <davidlarrimore@gmail.com>
2015-09-06 21:20:43 -0400

    Setup basic admin login form

---
+++ twoweeks/static/js/adminApp.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'userControllers'
+]);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/usersTable', {
+        templateUrl: '/static/partials/usersTable.html',
+        controller: 'UserListController'
+    }).
+    when('/usersView/:userId', {
+        templateUrl: '/static/partials/usersView.html',
+        controller: 'UserViewController'
+    }).
+    when('/usersForm', {
+        templateUrl: '/static/partials/usersForm.html',
+        controller: 'UserFormController'
+    }).
+    when('/usersEditForm/:userId', {
+        templateUrl: '/static/partials/usersEditForm.html',
+        controller: 'UserEditFormController'
+    }).
+    when('/userDelete/:userId', {
+        templateUrl: '/static/partials/usersTable.html',
+        controller: 'UserDeleteController'
+    }).
+    otherwise({
+        redirectTo: '/usersTable'
+    });
+}]);
+
+
