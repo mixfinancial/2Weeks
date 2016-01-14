b71eb45a26eb90f27607b8a786456785a1419238
David Larrimore <davidlarrimore@gmail.com>
2016-01-14 14:13:13 -0500

    Testing configs

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from twoweeks import app as application
-#import twoweeks.config as config
+import twoweeks.config as config
 
-application.run(debug=True, host='0.0.0.0')
+#application.run(debug=config.DEBUG, host=config.HOST)
+application.run(debug=config.DEBUG)
