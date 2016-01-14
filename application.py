fe5884227d14399e8610dbf2ab15006abd1a4dd0
David Larrimore <davidlarrimore@gmail.com>
2016-01-13 23:52:09 -0500

    testing

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
 from twoweeks import app as application
-import twoweeks.config as config
+#import twoweeks.config as config
 
-application.run(debug=config.DEBUG, host=config.HOST)
+application.run(debug=True, host='0.0.0.0')
